64deabd82e8ed8acfa76ed67b4cd797859f5a84d
DevC23 <156999220+DevC23@users.noreply.github.com>
2024-12-14 04:57:55 -0800

    feat: add pokemonList, display info on webpage and in console
    *Added a "pokemonList" array listing each name, height, weight, and type
    *Implemented method to show array output on webpage and in console logs

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,23 @@
-alert('Hello World!');
+//Pokemon list array with Name, Height, Weight, and Type displayed on webpage and in console//
 
-let favoriteFood = 'Pizza';
-document.write(favoriteFood);
+let pokemonList = [
+    {name: 'Bulbasaur', height: 0.7, weight: 6.9, type: ['Grass', 'Poison']},
+    {name: 'Charmander', height: 0.6, weight: 8.5, type: ['Fire']},
+    {name: 'Squirtle', height: 0.5, weight: 9, type: ['Water']}, 
+];
+document.write(`
+    Pokemon 1 - ${pokemonList[0].name} | Height: ${pokemonList[0].height}m | Weight: ${pokemonList[0].weight}kg |
+    Type: ${pokemonList[0].type[0]}, ${pokemonList[0].type[1]}<br>
+    Pokemon 2 - ${pokemonList[1].name} | Height: ${pokemonList[1].height}m | Weight: ${pokemonList[1].weight}kg |
+    Type: ${pokemonList[1].type[0]}<br>
+    Pokemon 3 - ${pokemonList[2].name} | Height: ${pokemonList[2].height}m | Weight: ${pokemonList[2].weight}kg |
+    Type: ${pokemonList[2].type[0]}<br>`
+);
+console.log(`
+    Pokemon 1 - ${pokemonList[0].name} | Height: ${pokemonList[0].height}m | Weight: ${pokemonList[0].weight}kg |
+    Type: ${pokemonList[0].type[0]}, ${pokemonList[0].type[1]}
+    Pokemon 2 - ${pokemonList[1].name} | Height: ${pokemonList[1].height}m | Weight: ${pokemonList[1].weight}kg |
+    Type: ${pokemonList[1].type[0]}
+    Pokemon 3 - ${pokemonList[2].name} | Height: ${pokemonList[2].height}m | Weight: ${pokemonList[2].weight}kg |
+    Type: ${pokemonList[2].type[0]}`
+);
